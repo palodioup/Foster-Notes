8a2744f09251942417bdf26c0965a0e518741fd6
Oladipupo Nathan Akanji <dipoakanji57@gmail.com>
2024-10-25 19:24:04 +0100

    Removed extra files

--- scripts/home.js
+++ scripts/home.js
@@ -1,21 +1,5 @@
 const inputField = document.getElementById("textInputArea");
-const fileSaveElement = document.getElementById("saveButton");
-const textAreaElement = document.getElementById("inputAreaTextElement");
-const addNewAreaButton = document.getElementById("addButton");
-const introMain = document.getElementById("explanationIntro");
 const text = inputField.value;
-const savedElement = [];
-let letters = 0;
-let files = 1;
-let worksheets = 0;
-
-introMain.innerHTML =
-  "Welcome to Foster Notes! Click on the save button below to save your work and click on the button beside it to add a new worksheet!";
-
-document.addEventListener("keypress", () => {
-  letters += 1;
-  console.log(letters);
-});
 
 function saveText() {
     function func_savedata(data) {
