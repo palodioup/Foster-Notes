1eb133dc3683f9a170a97e3d0f567d9a37c9b628
Oladipupo Nathan Akanji <dipoakanji57@gmail.com>
2024-10-25 22:10:39 +0100

    Added Media Queries

--- scripts/home.js
+++ scripts/home.js
@@ -1,19 +1,18 @@
 const inputField = document.getElementById("textInputArea");
 
 function saveText() {
-  let text = inputField.value
+  let text = inputField.value;
     function func_savedata(data) {
-    var string_data = JSON.stringify(data);
+    var string_data = data
     var file = new File([string_data], "savefile.txt", {
       type: "text;charset=utf-8",
     });
 
     var anchor = document.createElement("a");
     anchor.setAttribute("href", window.URL.createObjectURL(file));
-    anchor.setAttribute("download", "file.txt");
+    anchor.setAttribute("download", "savefile.txt");
     anchor.click();
     URL.revokeObjectURL(anchor.href);
   }
   func_savedata(text);
 }
-
