9cb660c8a4907d014dd1531e4a11e674f8317731
Oladipupo Nathan Akanji <dipoakanji57@gmail.com>
2024-10-31 17:51:40 +0100

    Organized work

--- scripts/dist/home.dev.js
+++ scripts/dist/home.dev.js
@@ -1,20 +1,23 @@
-const inputField = document.getElementById("textInputArea");
-const inputValue = inputField.value
+"use strict";
+
+var inputField = document.getElementById("textInputArea");
+var inputValue = inputField.value;
 
 function saveText() {
-  let text = inputField.value;
+  var text = inputField.value;
+
   function func_savedata(data) {
     var string_data = data;
     var file = new File([string_data], "note.txt", {
-      type: "text;charset=utf-8",
+      type: "text;charset=utf-8"
     });
-
     var anchor = document.createElement("a");
     anchor.setAttribute("href", window.URL.createObjectURL(file));
     anchor.setAttribute("download", "note.txt");
     anchor.click();
     URL.revokeObjectURL(anchor.href);
   }
+
   func_savedata(text);
-  window.location("/")
+  window.location("/");
 }
